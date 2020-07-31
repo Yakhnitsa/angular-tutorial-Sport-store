d32eafecdbf0ca59edf9e90de461917d44cf056d
Yuriy work-pc <transpmailbox@gmail.com>
2020-07-31 18:14:45 +0300

    Add pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store.component';
+import {ProductRepository} from './model/product.repository';
+import {StaticDataSource} from './model/static.datasource';
+import {ModelModule} from './model/model.module';
+import {StoreModule} from './store/store.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, StoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
